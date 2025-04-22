d885edd940e5aaa1cf2fa6d8e8991f9dd37c44c7
Deep Patel <deepp092@gmail.com>
2025-04-22 11:30:13 -0400

    impl std:error::Error on TimeErr

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TimeErr {
     /// Unable to floor the time
@@ -10,4 +12,24 @@ pub enum TimeErr {
     FrameTooLarge,
     /// Custom
     Other(String),
+}
+
+impl Display for TimeErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for TimeErr {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self)
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        self.source()
+    }
 }
